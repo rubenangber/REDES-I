d077b631184338b45dff69928164ed541f2fe566
oscaarrrhernandez <oscaarrhernandez451@gmail.com>
2023-11-09 17:06:55 +0100

    Co-authored-by: rubenangber <rubenangber@users.noreply.github.com>

---
+++ clientetcp.c
@@ -0,0 +1,62 @@
+/*
+** Fichero: clientetcp.c
+** Autores:
+** Rubén Angoso Berrocal DNI 70958754M
+** Óscar Hernández Hernández DNI 70918137Y
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <time.h>
+
+#define PUERTO 17278
+
+int main(int argc, char const *argv[])
+{
+	struct sockaddr_in myADDR;		 // Direccion del cliente
+	struct sockaddr_in serverADDR; // Direccion del servidor
+	int s, errcode;								 // Descriptor del socket
+	struct addrinfo hints, *res;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage:  %s <remote host>\n", argv[0]);
+		exit(1);
+	}
+
+	int s = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM porque es TCP
+	if (s == -1)
+	{
+		perror("Error al crear el socket");
+		fprintf(stderr, "%s:unable to create socket TPC\n", argv[0]);
+		exit(1);
+	}
+
+	memset((char *)&myADDR, 0, sizeof(struct sockaddr_in));
+	memset((char *)&serverADDR, 0, sizeof(struct sockaddr_in));
+
+	serverADDR.sin_family = AF_INET;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+
+	errcode = getaddrinfo(argv[1], NULL, &hints, &res);
+	if (errcode != 0)
+	{
+		fprintf(stderr, "%s: No es posible resolver la IP de %s\n", argv[0], argv[1]);
+		exit(1);
+	}
+	else
+	{
+		serverADDR.sin_addr.s_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr.s_addr;
+	}
+
+	return 0;
+}
---
+++ servidor.c
@@ -0,0 +1,6 @@
+/*
+** Fichero: servidor.c
+** Autores:
+** Rubén Angoso Berrocal DNI 70958754M
+** Óscar Hernández Hernández DNI 70918137Y
+*/
