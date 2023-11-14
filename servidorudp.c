af7da936d55789c491a45c35ea0bcaec500229b8
rubenangber <rubigrowl@gmail.com>
2023-11-14 13:12:40 +0100

    UDP FUNCIONA

--- servidorudp.c
+++ servidorudp.c
@@ -1,3 +1,10 @@
+/*
+** Fichero: clientetcp.c
+** Autores:
+** Rubén Angoso Berrocal. DNI 70958754M
+** Óscar Hernández Hernández. DNI 70918137Y
+*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/errno.h>
